eb0d242ee2f60887083a13c28021ac74f2a90cf1
Erid Bardhaj <erid.bardhaj@gmail.com>
2020-09-10 17:56:58 +0200

    Init SwiftUI App

---
+++ NanoBrewery/NanoBreweryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoBreweryApp.swift
+//  NanoBrewery
+//
+//  Created by Erid Bardhaj on 9/10/20.
+//
+
+import SwiftUI
+
+@main
+struct NanoBreweryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
