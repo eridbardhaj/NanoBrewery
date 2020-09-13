f242a7af8da38487e33a972553f9adc623c4c41a
Erid Bardhaj <erid.bardhaj@gmail.com>
2020-09-14 01:43:22 +0200

    Open Beer initializer to favor previews

--- NanoBrewery/Models/Beer.swift
+++ NanoBrewery/Models/Beer.swift
@@ -15,6 +15,7 @@ struct Beer: Codable, Equatable {
     let id: Int
     let name: String
     let tagline: String
+    let firstBrewed: String
     let description: String
     let percentage: Double
     let imageUrlString: String
@@ -27,6 +28,7 @@ struct Beer: Codable, Equatable {
         case id
         case name
         case tagline
+        case firstBrewed    = "first_brewed"
         case description
         case percentage     = "abv"
         case imageUrlString = "image_url"
@@ -38,6 +40,7 @@ struct Beer: Codable, Equatable {
         id = try values.decode(Int.self, forKey: .id)
         name = try values.decode(String.self, forKey: .name)
         tagline = try values.decode(String.self, forKey: .tagline)
+        firstBrewed = try values.decode(String.self, forKey: .firstBrewed)
         description = try values.decode(String.self, forKey: .description)
         percentage = try values.decode(Double.self, forKey: .percentage)
         imageUrlString = try values.decode(String.self, forKey: .imageUrlString)
@@ -48,16 +51,29 @@ struct Beer: Codable, Equatable {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(id, forKey: .id)
         try container.encode(name, forKey: .name)
+        try container.encode(tagline, forKey: .tagline)
+        try container.encode(firstBrewed, forKey: .firstBrewed)
         try container.encode(description, forKey: .description)
         try container.encode(percentage, forKey: .percentage)
         try container.encode(imageUrlString, forKey: .imageUrlString)
         try container.encode(foodPairing, forKey: .foodPairing)
     }
     
-    // MARK: - Helpers
-    // MARK: Handy Conversions
-    
-    var tags: [String] {
-        tagline.components(separatedBy: ". ")
+    init(id: Int,
+         name: String,
+         tagline: String,
+         firstBrewed: String,
+         description: String,
+         percentage: Double,
+         imageUrlString: String,
+         foodPairing: [String]) {
+        self.id = id
+        self.name = name
+        self.tagline = tagline
+        self.firstBrewed = firstBrewed
+        self.description = description
+        self.percentage = percentage
+        self.imageUrlString = imageUrlString
+        self.foodPairing = foodPairing
     }
 }
