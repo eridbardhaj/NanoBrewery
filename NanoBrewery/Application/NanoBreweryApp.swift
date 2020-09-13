45d5ea34cce0bf977a807c6847c5e56c716eb7fa
Erid Bardhaj <erid.bardhaj@gmail.com>
2020-09-14 01:50:30 +0200

    Link to BeerList and dump ContentView

--- NanoBrewery/Application/NanoBreweryApp.swift
+++ NanoBrewery/Application/NanoBreweryApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NanoBreweryApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BeerListView(viewModel: BeerListViewModel())
         }
     }
 }
