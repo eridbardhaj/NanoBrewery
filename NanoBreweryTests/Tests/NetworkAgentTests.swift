ee5412f1af2629e334aa2cbb6ac1231a422fc93a
Erid Bardhaj <erid.bardhaj@gmail.com>
2020-09-16 03:53:42 +0200

    Write tests for NetworkAgent

---
+++ NanoBreweryTests/Tests/NetworkAgentTests.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkAgentTests.swift
+//  NanoBreweryTests
+//
+//  Created by Erid Bardhaj on 9/16/20.
+//
+
+import XCTest
+import Combine
+@testable import NanoBrewery
+
+class NetworkAgentTests: XCTestCase {
+
+    // MARK: - Properties
+    // MARK: Mutable
+
+    var agent: NetworkAgent!
+    var urlSessionMock: URLSessionMock!
+
+    // MARK: - Test lifecycle
+
+    override func setUp() {
+        super.setUp()
+
+        setupMocks()
+    }
+
+    // MARK: - Setups
+
+    private func setupMocks() {
+        urlSessionMock = URLSessionMock()
+
+        agent = NetworkAgent(urlSession: urlSessionMock)
+    }
+
+    // MARK: - Tests
+
+    func testRun_dataTaskPublisherCalled() {
+        let dummyRequest = URLRequest(url: "www.example.com".requiredURL)
+        let _: AnyPublisher<Response<Beer>, Error> = agent.run(dummyRequest)
+
+        XCTAssertEqual(self.urlSessionMock.calledCount.dataTaskPublisher, 1)
+    }
+}
+
